ba21e17eb4afcb811c49e7aa2e81649c133e33ec
moei <chan.moeri@gmail.com>
2015-10-11 21:34:15 +0000

    pages now load again

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Blog::Application.routes.draw do
   devise_for :users
-  get "posts/index"
 
-  resources :posts
-
-  root 'posts#index'
+  root 'devise/sessions#new'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
