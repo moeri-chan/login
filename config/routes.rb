f76c1d177096141c43d29014853f208ccf9eb87a
moei <chan.moeri@gmail.com>
2015-10-11 21:39:03 +0000

    got devise routes working correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Blog::Application.routes.draw do
   devise_for :users
-
-  root 'devise/sessions#new'
-  
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
